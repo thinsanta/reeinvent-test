4f7eb8693175a81f612cf8406e161a9b647182e7
Fatlum Haliti <fatlum-haliti@outlook.com>
2023-11-28 16:16:35 +0100

    nothing is working yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
-
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [synonyms, setSynonyms] = useState([]);
+  const [word, setWord] = useState();
 
   const getSynonyms = async() =>{
     try {
       
-      const response = fetch("http://localhost:5000/synonyms")
+      const response = await fetch("/synonyms")
       if (!response) {
         console.log("Respsonse is not ok! " + response)
       }
@@ -18,12 +21,42 @@ function App() {
     }
   }
 
+  const addSynonym = async() =>{
+
+    const obj = {
+      "word" : word,
+      "synonyms" : synonyms
+    }
+
+    try {
+      const response = await fetch("/addsynonym",{
+        
+        method: 'POST',
+        body: JSON.stringify(obj)
+  
+      })
+
+      if (!response) {
+        console.log("Response is not ok!: " + response)
+      }
+      console.log(response)
+    } catch (error) {
+      console.log("Error adding synonym: " + error)
+    }
+  }
+
+  let inputHandler = (e) => {
+    //convert input text to lower case
+    let lowerCase = e.target.value.toLowerCase();
+    setWord(lowerCase);
+  };
+
 
   return (
     <div className="App">
-       <label for="synonym">Synonym:</label>
-        <input type="text" id="synonym" name="synonym" />
-        <button type="submit">Add</button>
+       <label htmlFor="word">Word:</label>
+        <input onChange={inputHandler} type="text" id="word" name="word" />
+        <button onClick={()=>addSynonym()} type="submit">Add</button>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')//add this
 
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
 app.get('/synonyms', (req, res) =>{
     res.send('Hello World!')
+})
+
+app.post('/addsynonym', (req, res) =>{
+    console.log(req.body)
 })
 
 
